771a2b1ea0c9cfe50baa4d175594ce4982b1e4fb
chrismcglynn <chris@mcglynnenterprises.com>
2018-12-26 15:16:15 -0700

    connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect('mongodb://chris:test123@ds215172.mlab.com:15172/gql-app');
+mongoose.connection.once('open', () => {
+  console.log('connected to database!');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
