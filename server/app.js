88aea0f5fbbe3f4977bec88f81a2e25ca2becfe9
chrismcglynn <chris@mcglynnenterprises.com>
2018-12-28 11:28:04 -0700

    make queries to graphql

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://chris:test123@ds215172.mlab.com:15172/gql-app');
